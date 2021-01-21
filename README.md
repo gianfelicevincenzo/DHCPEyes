2d30d8fff32a7f9b71c9e013ac548248f099b3e2
vincenzogianfelice <developer.vincenzog@gmail.com>
2021-01-22 00:09:09 +0100

    Cleaning and add possibility launch with python3

--- README.md
+++ README.md
@@ -3,17 +3,33 @@
 
 <img 
 src="https://raw.githubusercontent.com/vincenzogianfelice/DHCPEyes/master/media/demo.png"
-alt="Demo Usage"
-align="middle"
+alt="DemoImage"
 />
 
 # Installation
-###### Require python2.7
+###### Require python2.7 (also python3.5)
 
 ```
 pip2 install -r requirements.txt
 ```
+
 # Usage
+``` ____  __  ____________  ______
+   / __ \/ / / / ____/ __ \/ ____/_  _____  _____
+  / / / / /_/ / /   / /_/ / __/ / / / / _ \/ ___/
+ / /_/ / __  / /___/ ____/ /___/ /_/ /  __(__  )
+/_____/_/ /_/\____/_/   /_____/\__, /\___/____/
+                              /____/
+
+        * DHCP Passive Listener! (v1.0) *
+
+Usage: ./dhcpeyes.py -i <interface>
+
+Optional:
+     -o <arg>  File Output Save
+     -t <arg>  REQUEST types: DHCPR (request),  DHCPD (discover)
+```
+######Examples
 ```
 ./dhcpeyes.py -i wlan0 -t DHCPR  # Intercept only DHCPREQUEST
 ./dhcpeyes.py -i wlan0  # Intercept all
--- dhcpeyes.py
+++ dhcpeyes.py
@@ -1,107 +1,130 @@
 #!/usr/bin/env python2.7
 #  Coded by vincenzogianfelice <developer.vincenzog@gmail.com> with <3
+VERSION='v1.0'
+
 import base64
 import sys
 import time
+import os
 from termcolor import colored
-from scapy.all import *
-from os import getuid
+from scapy.all import Ether,IP,DHCP,sniff
 
-LOGO='ICAgIF9fX18gIF9fICBfX19fX19fX19fX18gIF9fX19fXyAgICAgICAgICAgICAgIAogICAvIF9fIFwvIC8gLyAvIF9fX18vIF9fIFwvIF9fX18vXyAgX19fX18gIF9fX19fCiAgLyAvIC8gLyAvXy8gLyAvICAgLyAvXy8gLyBfXy8gLyAvIC8gLyBfIFwvIF9fXy8KIC8gL18vIC8gX18gIC8gL19fXy8gX19fXy8gL19fXy8gL18vIC8gIF9fKF9fICApIAovX19fX18vXy8gL18vXF9fX18vXy8gICAvX19fX18vXF9fLCAvXF9fXy9fX19fLyAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC9fX19fLyAgICAgICAgICAgICAK'
+## DHCP Message Type - RFC2132/Page-4 (https://tools.ietf.org/html/rfc2132#page-4) ##
+## Per il momento, verranno utilizzati solo DHCPREQUEST e DHCPDISCOVER.
+## Presto verra' aggiunta la possibilita' di intercettare il DHCPOFFER
+## Stay Tuned! ;)
+DHCPDISCOVER=1
+DHCPOFFER=2
+DHCPREQUEST=3
+DHCPDECLINE=4
+DHCPACK=5
+DHCPNAK=6
+DHCPRELEASE=7
+DHCPINFORM=8
+
+## DHCPOptions of Scapy
+## (https://github.com/secdev/scapy/blob/master/scapy/layers/dhcp.py)
+## `at line 112`. Actual version of scapy is 2.4.4.
+MESSAGE_DHCP='message-type'
+REQUEST_ADDRESS='requested_addr'
+ADDRESS_SERVER_DHCP='server_id'
+VENDOR='vendor_class_id'
+HOSTNAME='hostname'
+
+def logo():
+    LOGO='ICAgIF9fX18gIF9fICBfX19fX19fX19fX18gIF9fX19fXyAgICAgICAgICAgICAgIAogICAvIF9fIFwvIC8gLyAvIF9fX18vIF9fIFwvIF9fX18vXyAgX19fX18gIF9fX19fCiAgLyAvIC8gLyAvXy8gLyAvICAgLyAvXy8gLyBfXy8gLyAvIC8gLyBfIFwvIF9fXy8KIC8gL18vIC8gX18gIC8gL19fXy8gX19fXy8gL19fXy8gL18vIC8gIF9fKF9fICApIAovX19fX18vXy8gL18vXF9fX18vXy8gICAvX19fX18vXF9fLCAvXF9fXy9fX19fLyAgCiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC9fX19fLyAgICAgICAgICAgICAK'
+
+    print(base64.b64decode(LOGO).decode('ascii'))
+    print('\t* DHCP Passive Listener! ('+VERSION+') *')
+    print('')
 
 def help():
-    print 'Usage: {} -i <interface>'.format(sys.argv[0])
-    print ''
-    print 'Optional:'
-    print '     -o <arg>  File Output Save'
-    print '     -t <arg>  REQUEST types: DHCPR (request),  DHCPD (discover)'
+    print('Usage: {} -i <interface>'.format(sys.argv[0]))
+    print
+    print('Optional:')
+    print('     -o <arg>  File Output Save')
+    print('     -t <arg>  REQUEST types: DHCPR (request),  DHCPD (discover)')
 
-def dhcp_discover(pkts):
-    data=list()
+def dhcp_options_search(pkts):
+    data=[pkts.getlayer(DHCP).options[0][1]]
     for ex in pkts.getlayer(DHCP).options:
-        if 'hostname' in ex:
-            data.append(('hostname', ex[1]))
-        elif 'vendor_class_id' in  ex:
-            data.append(('vendor_class_id', ex[1]))
+        if REQUEST_ADDRESS in ex:
+            data.append((REQUEST_ADDRESS, ex[1]))
+        elif ADDRESS_SERVER_DHCP in ex:
+            data.append((ADDRESS_SERVER_DHCP, ex[1]))
+        elif VENDOR in ex:
+            data.append((VENDOR, ex[1]))
+        elif HOSTNAME in ex:
+            data.append((HOSTNAME, ex[1]))
 
     return data
 
-def dhcp_request(pkts):
-    data=list()
-    for ex in pkts.getlayer(DHCP).options:
-        if 'requested_addr' in ex:
-            data.append(('requested_addr', ex[1]))
-        elif 'server_id' in ex:
-            data.append(('server_id', ex[1]))
-        elif 'vendor_class_id' in ex:
-            data.append(('vendor_class_id', ex[1]))
-        elif 'hostname' in ex:
-            data.append(('hostname', ex[1]))
-
-    return data
-
-def show_data(pkts):
-    data=list()
-    mac=pkts.getlayer(Ether).src
-    ip='???'
-    vendor_class_id='???'
-    ip_server_dhcp='???'
+def parser_packet(pkts):
+    mac_addr=pkts.getlayer(Ether).src
+    address='???'
+    vendor='???'
+    address_server_dhcp='???'
     hostname='???'
 
-    type_request=pkts.getlayer(DHCP).options[0][1]
+    data=dhcp_options_search(pkts)
 
-    ##KEY VALUE FOR PRINT##
-    k_addr=colored('addr', 'yellow', attrs=['bold'])
-    k_host=colored('host', 'yellow', attrs=['bold'])
+    type_option=data[0]
+    del data[0]
 
-    if type_request == 1:
-        if (type_dhcp == 'DHCPD' or not type_dhcp):
-            data=dhcp_discover(pkts)
-    elif type_request == 3:
-        if (type_dhcp == 'DHCPR' or not type_dhcp):
-            data=dhcp_request(pkts)
+    ## Check in base al valore dell'opzione '-t' e dal tipo di pacchetto
+    ## in entrata
+    if type_option == DHCPDISCOVER and (type_dhcp == 'DHCPD' or not type_dhcp):
+        option_dhcp='DHCPDISCOVER'
+    elif type_option == DHCPREQUEST and (type_dhcp == 'DHCPR' or not type_dhcp):
+        option_dhcp='DHCPREQUEST'
+    else:
+        return
 
-    if (data):
-        for ex in data:
-            if 'requested_addr' in ex:
-                ip=ex[1]
-            elif 'server_id' in ex:
-                ip_server_dhcp=ex[1]
-            elif 'vendor_class_id' in ex:
-                vendor_class_id=ex[1]
-            elif 'hostname' in ex:
-                hostname=ex[1]
+    time_capture=time.strftime("%d/%m/%y %H:%M")
+    for ex in data:
+        if REQUEST_ADDRESS in ex:
+            address=ex[1]
+        elif ADDRESS_SERVER_DHCP in ex:
+            address_server_dhcp=ex[1]
+        elif VENDOR in ex:
+            vendor=ex[1].decode('utf8')
+        elif HOSTNAME in ex:
+            hostname=ex[1].decode('utf8')
 
     if (file_out):
         with open(file_out,'a+') as fd:
-            if (type_request==1 and type_dhcp == 'DHCPD' or not type_dhcp):
-                t='DISCOVER'
-            elif (type_request==3 and type_dhcp == 'DHCPR' or not type_dhcp):
-                t='REQUEST'
-            else:
-                return
-
-            fd.write('{}: {} server_dhcp={} mac={} vendor={} address={} hostname={}'.format(time.strftime("%d-%m-%y-%H-%M"), t, ip_server_dhcp, mac, vendor_class_id, ip, hostname))
+            fd.write('{}: {} server_dhcp={} mac={} vendor={} address={} hostname={}'.format(time_capture, option_dhcp, address_server_dhcp, mac_addr, vendor, address, hostname))
             fd.write('\n')
 
-    if (type_request == 1 and (type_dhcp == 'DHCPD' or not type_dhcp)):
-        print '[{}] DHCP DISCOVER ({} ({})) {}: {}'.format(time.strftime("%d/%m/%y %H:%M"), mac, vendor_class_id, k_host, hostname)
-    elif (type_request == 3 and (type_dhcp == 'DHCPR' or not type_dhcp)):
-        print '[{}] DHCP REQUEST ({}) CLIENT -> [{} ({})]: {}: {} {}: {}'.format(time.strftime("%d/%m/%y %H:%M"), colored(ip_server_dhcp,'blue',attrs=['bold']), mac, vendor_class_id, k_addr, ip, k_host, hostname)
+    if (type_option == DHCPDISCOVER):
+        output_format='[{}] {} ({} ({})) {}: {}'.format(time_capture,
+                                                        option_dhcp,
+                                                        mac_addr,
+                                                        vendor,
+                                                        colored('host', 'yellow', attrs=['bold']),
+                                                        hostname)
+    elif (type_option == DHCPREQUEST):
+        output_format='[{}] {} ({}) CLIENT -> [{} ({})] {}: {} {}: {}'.format(time_capture,
+                                                                              option_dhcp,
+                                                                              colored(address_server_dhcp, 'blue', attrs=['bold']),
+                                                                              mac_addr,
+                                                                              vendor,
+                                                                              colored('addr', 'yellow', attrs=['bold']),
+                                                                              address,
+                                                                              colored('host', 'yellow', attrs=['bold']),
+                                                                              hostname)
+
+    print(output_format)
 
 if __name__ == '__main__':
     interface=None
     type_dhcp=None
     file_out=None
 
-    print base64.b64decode(LOGO)
-    print '\t* DHCP Passive Listener! *'
-    print
+    logo()
 
-    
     op=sys.argv[1::2]
     val=sys.argv[2::2]
-
     if (len(sys.argv) < 2 or len(op) != len(val)):
         help()
         sys.exit(1)
@@ -119,8 +142,9 @@ if __name__ == '__main__':
             interface=val[index_op]
 
     ## Check root permissions
-    if (getuid() != 0):
-        print 'Please run as root'
-        sys.exit(1)
+    if (os.name != 'win32'):
+        if (os.getuid() != 0):
+            print('Please run as root')
+            sys.exit(1)
 
-    sniff(iface=interface, prn=show_data,store=False, filter=('udp port 67 and port 68'))
+    sniff(iface=interface, prn=parser_packet, store=False, filter=('udp port 67 and port 68'))
